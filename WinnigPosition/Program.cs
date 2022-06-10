d134342724870d1f84b81968468eb3b182bc4211
AtulSikarwal <atulsikarwal5.com>
2022-06-10 14:54:50 +0530

    The code for Repeat at the player till position 100

---
+++ WinnigPosition/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Snake_LadderProblem
+{
+    public class Program
+    {
+        public static void Main(String[]args)
+        {
+            Snake_Ladder Result=new Snake_Ladder();
+            Result.SnakeAndLadder();
+        }
+    }
+}
